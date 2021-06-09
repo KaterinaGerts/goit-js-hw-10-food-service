97d0f35e9940110f5f2bae7b1eddb69fab6922fb
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-06-09 19:38:53 +0300

    hw is done

--- src/index.js
+++ src/index.js
@@ -4,34 +4,59 @@ import markUp from '../menu.json';
 //console.log(markUp);
 import '../src/styles.css';
 
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-}; 
-
-const toggleTheme = document.getElementById('theme-switch-toggle');
 
 const menuEl = document.querySelector('.js-menu');
 const createMenuEl = createMenuMarkUp( markUp);
 menuEl.insertAdjacentHTML('beforeend', createMenuEl);
 
+
 function createMenuMarkUp (markUp) { 
   return menuMarkUpTpl (markUp);
 }
 
+const toggleTheme = document.getElementById('theme-switch-toggle');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const BODY = document.body;
+const saveTheme = localStorage.getItem('theme');
+
 savedTheme()
+
 toggleTheme.addEventListener('change', onToggleTheme);
 
-function onToggleTheme() {
-  document.body.classList.toggle(Theme.DARK)
-  localStorage.Theme = document.body.className || Theme.LIGHT;   
+function onToggleTheme(e) {
+  e.preventDefault();
+
+  if(e.target.checked) {
+    BODY.classList.add(Theme.DARK)
+    BODY.classList.remove(Theme.LIGHT)
+    localStorage.setItem('theme', Theme.DARK)
+    toggleTheme.checked = true; 
+  } else {
+    BODY.classList.add(Theme.LIGHT)
+    BODY.classList.remove(Theme.DARK)
+    localStorage.setItem('theme', Theme.LIGHT) 
+    toggleTheme.checked = false;
+  }    
 };
 
+
 function savedTheme() {
-  if(toggleTheme.classList.add('checked')) {
-  localStorage.Theme.DARK
-  } else {  
-    toggleTheme.classList.remove('checked')  
-    localStorage.Theme.LIGHT
-}
+  if(saveTheme === Theme.DARK) {
+    BODY.classList.remove(Theme.LIGHT)
+    BODY.classList.add(Theme.DARK)
+    localStorage.setItem('theme', Theme.DARK)
+    toggleTheme.checked = true; 
+    return;
+  }  
+  
+  if(saveTheme === Theme.LIGHT) {
+    BODY.classList.remove(Theme.DARK)
+    BODY.classList.add(Theme.LIGHT)
+    localStorage.setItem('theme', Theme.LIGHT) 
+    toggleTheme.checked = false;
+    return;
+  }
 }; 
